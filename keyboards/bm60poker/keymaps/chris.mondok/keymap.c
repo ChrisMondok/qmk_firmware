7dcd5a4a779ba62d27f906d9fe6d0b57e76ae3c1
Chris Mondok <chris.mondok@gmail.com>
2021-01-01 12:26:37 -0500

    Rearrange bottom right four keys

--- keyboards/bm60poker/keymaps/chris.mondok/keymap.c
+++ keyboards/bm60poker/keymaps/chris.mondok/keymap.c
@@ -21,7 +21,7 @@
 #include "rgb_matrix_util.c"
 
 #define RGB_DISABLE_WHEN_USB_SUSPENDED true
-#define KC_COMPOSE KC_RGUI
+#define KC_COMPOSE KC_RALT
 
 enum layers {
   _querty,
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         LT(1,KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              KC_RALT,   KC_COMPOSE,   KC_APP,   KC_RCTL
+        KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              KC_RGUI,  KC_COMPOSE, KC_APP,  KC_RCTL
     ),
     [_fn] = LAYOUT_60_ansi(
         KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
