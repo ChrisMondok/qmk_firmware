33de3976203c3f61e7a3d749347e1b93739c89be
Chris Mondok <chris.mondok@gmail.com>
2021-01-01 14:32:16 -0500

    Add arrow keys to gaming layer

--- keyboards/bm60poker/keymaps/chris.mondok/keymap.c
+++ keyboards/bm60poker/keymaps/chris.mondok/keymap.c
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              KC_RALT,   TG(_game),   KC_APP,     KC_RCTL
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
+        KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              TG(_game), KC_LEFT,  KC_DOWN, KC_RIGHT
     ),
     [_lighting] = LAYOUT_60_ansi(
         RGB_TOG,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -88,8 +88,14 @@ void rgb_matrix_indicators_user(void) {
     rgb_matrix_set_color(30, 0x00, rgb_matrix_config.hsv.v, 0x00);
     rgb_matrix_set_color(31, 0x00, rgb_matrix_config.hsv.v, 0x00);
 
-    // exit key in green
+    // arrow keys in green
+    rgb_matrix_set_color(52, 0x00, rgb_matrix_config.hsv.v, 0x00);
     rgb_matrix_set_color(58, 0x00, rgb_matrix_config.hsv.v, 0x00);
+    rgb_matrix_set_color(59, 0x00, rgb_matrix_config.hsv.v, 0x00);
+    rgb_matrix_set_color(60, 0x00, rgb_matrix_config.hsv.v, 0x00);
+
+    // exit key in cyan
+    rgb_matrix_set_color(57, 0x00, rgb_matrix_config.hsv.v, rgb_matrix_config.hsv.v);
   } else {
     colorize_keys_by_keycode();
   }
