ed1b6576d8f8f2a373b746c1a2e2ca51142cdbee
Chris Mondok <chris.mondok@gmail.com>
2021-01-02 10:10:19 -0500

    add a sleep button

--- keyboards/bm60poker/keymaps/chris.mondok/keymap.c
+++ keyboards/bm60poker/keymaps/chris.mondok/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         XXXXXXX,      KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17, XXXXXXX, KC_PGUP, KC_UP, KC_PGDN, KC_PSCR, XXXXXXX, XXXXXXX, KC_INS,
         KC_TRNS,       KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, TG(_game), XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_TRNS,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_END, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,
+        KC_TRNS,            KC_SLEP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_END, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,
         KC_TRNS,   KC_TRNS,   KC_TRNS,                      XXXXXXX,                   XXXXXXX,   TG(_lighting),   KC_TRNS,   KC_TRNS
     ),
     [_game] = LAYOUT_60_ansi(
@@ -139,6 +139,7 @@ void color_key(int led_index, uint16_t keycode) {
       rgb_matrix_set_color(led_index, 0x00, 0x00, hsv.v);
       return;
     case KC_PSCR:
+    case KC_SLEP:
       // yellow
       rgb_matrix_set_color(led_index, hsv.v, hsv.v, 0x00);
       return;
