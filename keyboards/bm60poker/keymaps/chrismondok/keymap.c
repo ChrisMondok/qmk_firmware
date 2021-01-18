918d4d66c77f05de77d8aee2560875d35e93fb61
Chris Mondok <chris.mondok@gmail.com>
2021-01-18 12:04:12 -0500

    Rename enum values. Also how the hell did I misspell qwerty?

--- keyboards/bm60poker/keymaps/chrismondok/keymap.c
+++ keyboards/bm60poker/keymaps/chrismondok/keymap.c
@@ -27,58 +27,58 @@
 #define KC_COMPOSE KC_RALT
 
 enum layers {
-  _querty,
-  _fn,
-  _game,
-  _lighting,
-  _num_layers,
+  LAYER_QWERTY,
+  LAYER_FN,
+  LAYER_GAME,
+  LAYER_LIGHTING,
+  LAYER_MAX,
 };
 
 enum custom_codes {
-  _rgb_plain = SAFE_RANGE
+  KC_CUSTOM_RGB_PLAIN = SAFE_RANGE
 };
 
 enum hid_commands {
-  _reset_led_groups = 0x00,
-  _set_led_group,
-  _set_layer,
+  HID_CMD_RESET_LED_GROUPS = 0x00,
+  HID_CMD_SET_LED_GROUP,
+  HID_CMD_SET_LAYER,
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_querty] = LAYOUT_60_ansi(
+const uint16_t PROGMEM keymaps[LAYER_MAX][MATRIX_ROWS][MATRIX_COLS] = {
+    [LAYER_QWERTY] = LAYOUT_60_ansi(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         LT(1,KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              KC_RGUI,  KC_COMPOSE, KC_APP,  KC_RCTL
     ),
-    [_fn] = LAYOUT_60_ansi(
+    [LAYER_FN] = LAYOUT_60_ansi(
         KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         XXXXXXX,      KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17, XXXXXXX, KC_PGUP, KC_UP, KC_PGDN, KC_PSCR, XXXXXXX, XXXXXXX, KC_INS,
-        KC_TRNS,       KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, TG(_game), KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_TRNS,       KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, TG(LAYER_GAME), KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_TRNS,            KC_SLEP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_END, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,
-        KC_TRNS,   KC_TRNS,   KC_TRNS,                      XXXXXXX,                   XXXXXXX,   TG(_lighting),   KC_TRNS,   KC_TRNS
+        KC_TRNS,   KC_TRNS,   KC_TRNS,                      XXXXXXX,                   XXXXXXX,   TG(LAYER_LIGHTING),   KC_TRNS,   KC_TRNS
     ),
-    [_game] = LAYOUT_60_ansi(
+    [LAYER_GAME] = LAYOUT_60_ansi(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
-        KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              TG(_game), KC_LEFT,  KC_DOWN, KC_RIGHT
+        KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              TG(LAYER_GAME), KC_LEFT,  KC_DOWN, KC_RIGHT
     ),
-    [_lighting] = LAYOUT_60_ansi(
+    [LAYER_LIGHTING] = LAYOUT_60_ansi(
         RGB_TOG,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        TG(_lighting),       XXXXXXX, RGB_SAI, XXXXXXX, XXXXXXX, XXXXXXX, RGB_HUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        TG(LAYER_LIGHTING),       XXXXXXX, RGB_SAI, XXXXXXX, XXXXXXX, XXXXXXX, RGB_HUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_LSFT,            XXXXXXX, XXXXXXX, XXXXXXX, RGB_VAI, XXXXXXX, XXXXXXX, XXXXXXX, RGB_RMOD, RGB_MOD, XXXXXXX, KC_RSFT,
-        XXXXXXX,   XXXXXXX,   XXXXXXX,                      _rgb_plain,                              XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX
+        XXXXXXX,   XXXXXXX,   XXXXXXX,                      KC_CUSTOM_RGB_PLAIN,                              XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX
     ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if(!process_record_shift_lock(keycode, record)) return false;
   switch(keycode) {
-    case _rgb_plain:
+    case KC_CUSTOM_RGB_PLAIN:
       rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
       return false;
   }
@@ -110,10 +110,10 @@ static const LedGroup gaming_led_group = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  if(!layer_state_cmp(state, _game) && layer_state_cmp(layer_state_old, _game)) {
+  if(!layer_state_cmp(state, LAYER_GAME) && layer_state_cmp(layer_state_old, LAYER_GAME)) {
     // leaving game layer
     reset_led_groups();
-  } else if(layer_state_cmp(state, _game) && !layer_state_cmp(layer_state_old, _game)) {
+  } else if(layer_state_cmp(state, LAYER_GAME) && !layer_state_cmp(layer_state_old, LAYER_GAME)) {
     //entering game layer
     set_led_group(1, &gaming_led_group);
   }
@@ -123,15 +123,15 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
   switch(*data) {
-    case _reset_led_groups:
+    case HID_CMD_RESET_LED_GROUPS:
       reset_led_groups();
       break;
-    case _set_led_group:
+    case HID_CMD_SET_LED_GROUP:
       //format: [cmdNumber, groupNumber, …group]
       set_led_group(data[1], (LedGroup *) (data+2));
       break;
-    case _set_layer:
-      if(data[1] < _num_layers) {
+    case HID_CMD_SET_LAYER:
+      if(data[1] < LAYER_MAX) {
         layer_move(data[1]);
         break;
       }
@@ -157,13 +157,13 @@ void rgb_matrix_indicate_hue(int led_index, uint8_t hue) {
 }
 
 void rgb_matrix_indicators_user(void) {
-  if(!layer_state_cmp(layer_state, _game)) {
+  if(!layer_state_cmp(layer_state, LAYER_GAME)) {
     colorize_keys_by_keycode();
   }
 
   rgb_matrix_indicate_groups();
 
-  if(layer_state_cmp(layer_state, _lighting)) {
+  if(layer_state_cmp(layer_state, LAYER_LIGHTING)) {
     show_brightness_on_number_line();
   }
 }
@@ -173,7 +173,7 @@ void color_key(int led_index, uint16_t keycode) {
     case XXXXXXX:
       // black
       // don't turn off keys on the lighting layer.
-      if(!layer_state_cmp(layer_state, _lighting)) {
+      if(!layer_state_cmp(layer_state, LAYER_LIGHTING)) {
         rgb_matrix_set_color(led_index, 0x00, 0x00, 0x00);
       }
       return;
@@ -215,7 +215,7 @@ void color_key(int led_index, uint16_t keycode) {
       // red
       rgb_matrix_indicate_hue(led_index, 0x00);
       return;
-    case TG(_game):
+    case TG(LAYER_GAME):
       // green
       rgb_matrix_indicate_hue(led_index, 0x55);
       return;
